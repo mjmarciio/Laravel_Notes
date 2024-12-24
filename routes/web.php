fdfb5b7cd74cfe141bb39b94b30f39ea3e2f738d
Márcio Junior <marciomsgj@gmail.com>
2024-12-24 20:29:09 -0300

    aprendendo a ancorar links de rotas com blade

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::middleware([CheckIsNotLogged::class])->group(function(){
 // auth routes - user logged
 Route::middleware([CheckIsLogged::class])->group(function(){
     
-    Route::get('/',[MainController::class, 'index']);
-    Route::get('/newNote',[MainController::class, 'newNote']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/',[MainController::class, 'index'])->name('home');
+    Route::get('/newNote',[MainController::class, 'newNote'])->name('new');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 });
 
