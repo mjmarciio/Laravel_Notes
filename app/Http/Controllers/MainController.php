dfb899e7dfcaeb1de0c061999fdeffdb571025c8
Márcio Junior <marciomsgj@gmail.com>
2024-12-24 20:13:04 -0300

    aprendendo sobre middlewares em laravel

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    
+    public function index(){
+        echo "I'm inside the app!";
+        
+    }
+
+    public function newNote(){
+
+        echo "I'm creating a new note!";
+        
+    }
 
 
 
