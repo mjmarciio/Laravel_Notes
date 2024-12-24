2e4a9d5fdcc7bdabf4f219c19d8fb7f9daaafc7e
Márcio Junior <marciomsgj@gmail.com>
2024-12-24 10:43:32 -0300

    aprendendo sobre metodo post em rotas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,13 @@ class AuthController extends Controller
     public function login(){
         return view('login');
     }
+
+    public function loginSubmit(Request $request){
+        echo $request->input('text_username');
+        echo '<br>';
+        echo $request->input('text_password');
+    }
+
     public function logout(){
         echo 'logout';
     }
