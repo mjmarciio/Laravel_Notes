c30a0cd71687891f6357e15f1d01fce25e9cfa6a
Márcio Junior <marciomsgj@gmail.com>
2024-12-25 16:30:44 -0300

    aprendendo o trait de SoftDelete

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -141,8 +141,14 @@ class MainController extends Controller
         // $note->delete();
 
         // 2. soft delete
-        $note->deleted_at = date('Y-m-d H:i:s');
-        $note->save();
+        // $note->deleted_at = date('Y-m-d H:i:s');
+        // $note->save();
+
+        // 3. soft delete (property SoftDelete in model)
+        $note->delete();
+
+        // 4. soft delete (property SoftDelete in model)
+        // $note->forceDelete();
 
         // redirect to home
         return redirect()->route('home');
