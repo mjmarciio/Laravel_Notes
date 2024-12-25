ee96dd5f1fdb569a124bd18a481e3316e9d683ac
Márcio Junior <marciomsgj@gmail.com>
2024-12-25 15:01:51 -0300

    criando arquivo de Operations para organização de código

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -28,27 +29,15 @@ class MainController extends Controller
 
     public function editNote($id){
 
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
 
 
     }
 
     public function deleteNote($id){
 
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
 
-    }
-
-    private function decryptId($id){
-
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $er) {
-            
-            return redirect()->route('home');
-        }
-
-        return $id;
     }
 
 
---
+++ app/Services/Operations.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
+
+class Operations{
+
+    public static function decryptId($value){
+
+        try {
+            $value = Crypt::decrypt($value);
+        } catch (DecryptException $er) {
+            
+            return redirect()->route('home');
+        }
+
+        return $value;
+
+    }
+}
